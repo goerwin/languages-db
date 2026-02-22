c0b17c09801fa23c243d8d39ee994cb60984b863
Erwin G <erwingaitano@gmail.com>
2026-02-21 20:12:37 -0500

    chore: using import.meta.dirname for the dirname directly

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { validateConnectors, validateMdEntries } from './english/utils.ts';
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 (
   [
     [
-      path.resolve(dirname, './english/adjectives.md'),
+      path.resolve(import.meta.dirname, './english/adjectives.md'),
       validateMdEntries,
       [
         { name: 'pho', unique: true, required: true },
@@ -17,14 +14,17 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
       ],
     ],
     [
-      path.resolve(dirname, './english/common-idioms.md'),
+      path.resolve(import.meta.dirname, './english/common-idioms.md'),
       validateMdEntries,
       [
         { name: 'def', unique: true, required: true },
         { name: 'eg', unique: false, required: true },
       ],
     ],
-    [path.resolve(dirname, './english/connectors.md'), validateConnectors],
+    [
+      path.resolve(import.meta.dirname, './english/connectors.md'),
+      validateConnectors,
+    ],
   ] as const
 ).forEach(async ([filePath, validator, args]) =>
   validator(await fs.readFile(filePath, 'utf-8'), args)
